666f867828bd348ee7a3dc7940b5889d6b540991
Yuss9 <yurtseven68210@hotmail.fr>
2023-11-08 13:33:05 +0100

    feat: now we need to give file name

--- main.cpp
+++ main.cpp
@@ -1,20 +1,32 @@
 #include <iostream>
 #include <opencv2/highgui.hpp>
 using namespace cv;
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " <nom-fichier-image>" << endl;
+    return 1;
+  }
+
   int old_value = 0;
   int value = 128;
-  namedWindow( "TP1");               // crée une fenêtre
-  createTrackbar( "track", "TP1", &value, 255, NULL); // un slider
-  Mat f = imread("lena.png");        // lit l'image "lena.png"
-  imshow( "TP1", f );                // l'affiche dans la fenêtre
-  while ( waitKey(50) < 0 )          // attend une touche
-  { // Affiche la valeur du slider
-    if ( value != old_value )
-    {
+  namedWindow("TP1");                // crée une fenêtre
+  createTrackbar("track", "TP1", &value, 255, NULL); // un slider
+  Mat f = imread(argv[1]);           // lit l'image spécifiée en paramètre
+  if (f.empty()) {
+    cout << "Impossible de charger l'image " << argv[1] << endl;
+    return 1;
+  }
+  imshow("TP1", f);                  // l'affiche dans la fenêtre
+
+  while (waitKey(50) < 0) {
+    if (value != old_value) {
       old_value = value;
-      std::cout << "value=" << value << std::endl;
+      cout << "value=" << value << endl;
     }
   }
-} 
+
+  return 0;
+}
