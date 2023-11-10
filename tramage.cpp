cdc18ecabe2e51dee63fd54251a43250a1553787
Yuss9 <yurtseven68210@hotmail.fr>
2023-11-10 13:50:39 +0100

    feat: do tramage floyd color

--- tramage.cpp
+++ tramage.cpp
@@ -61,6 +61,66 @@ void test_tramage_floyd_steinberg(String filename){
 
 }
 
+/*
+#################################################################### 
+*/
+
+
+cv::Mat tramage_floyd_steinberg_color(cv::Mat input) {
+  int rowSize = input.rows;
+  int columnSize = input.cols;
+
+    cv::Mat output(rowSize, columnSize, input.type());
+    output.setTo(255);
+
+    for (int row = 0; row < rowSize; row++) {
+        for (int col = 0; col < columnSize; col++) {
+            for (int channel = 0; channel < input.channels(); channel++) {
+                uchar pixel = input.at<Vec3b>(row, col)[channel];
+
+                // Calcul de la couleur la plus proche
+                uchar new_pixel = pixel > 127 ? 255 : 0;
+                output.at<Vec3b>(row, col)[channel] = new_pixel;
+
+                int error = pixel - new_pixel;
+
+                if (col + 1 < columnSize) input.at<Vec3b>(row, col + 1)[channel] += error * 5 / 16;
+                if (row + 1 < rowSize) {
+                    if (col - 1 >= 0) input.at<Vec3b>(row + 1, col - 1)[channel] += error * 3 / 16;
+                    input.at<Vec3b>(row + 1, col)[channel] += error * 3 / 16; // Ajustement ici
+                    if (col + 1 < columnSize) input.at<Vec3b>(row + 1, col + 1)[channel] += error * 1 / 16;
+                }
+            }
+        }
+    }
+    return output;
+}
+
+void test_tramage_floyd_steinberg_color(String filename){
+    Mat f = imread(filename);
+
+    if (f.empty()) {
+        cout << "Impossible de charger l'image " << filename << endl;
+        return;
+    }
+    namedWindow("Image initial");
+    imshow("Image initial", f);
+
+
+    Mat final_image = tramage_floyd_steinberg_color(f);
+    namedWindow("Image finale");
+    imshow("Image finale", final_image);
+
+
+    waitKey(0);
+
+}
+
+
+/*
+#################################################################### 
+*/
+
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -68,14 +128,14 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Mat f = imread(argv[1], IMREAD_GRAYSCALE);
+    Mat f = imread(argv[1]);
 
     if (f.empty()) {
         cout << "Impossible de charger l'image " << argv[1] << endl;
         return 1;
     }
 
-    test_tramage_floyd_steinberg(argv[1]);
+    test_tramage_floyd_steinberg_color(argv[1]);
 
     return 0;
 }
